81659d6a9395e252d9b719c1adef2cddb4a87964
turalcavad <turaltj@code.edu.az>
2023-02-16 19:40:50 +0400

    project configuration

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        xl: '1200px',
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
